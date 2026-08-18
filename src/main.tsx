f2e474cfa22e244bb1c27938fa783e0528457081
one-media-asia <peterjgreaney@icloud.com>
2026-08-18 18:42:53 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
+import ReactGA from "react-ga4";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactGA.initialize("G-DJBJE1TBX0");
+
+createRoot(document.getElementById("root")!).render(
+  <App />
+);
